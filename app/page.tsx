cb9e37c4a5e26f9a35f75f2db629cc3f10991e74
yubraj525 <yubrajkhatri525@gmail.com>
2026-01-14 14:38:14 +0545

    setup: setup next project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Welcome to My Home Page</h1>
+      <p>This is the default page of my Next.js app.</p>
+    </main>
+  );
+}
